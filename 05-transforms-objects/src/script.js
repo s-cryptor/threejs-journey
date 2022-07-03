23f7c7659e6d191accc69657b3b31a7e65fa59a1
s-cryptor <grisga2001@gmail.com>
2022-07-03 20:13:38 +0300

    add group and transform it

--- 05-transforms-objects/src/script.js
+++ 05-transforms-objects/src/script.js
@@ -10,10 +10,51 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'red' })
+)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'green' })
+)
+cube2.position.x = 2
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'blue' })
+)
+cube3.position.x = -2
+
+group.add(cube1)
+group.add(cube2)
+group.add(cube3)
+
+/**
+ * Position
+ */
+
+/**
+ * Scale
+ */
+
+/**
+ * Rotation
+ */
+
+
+/**
+ * Axes Helper
+ */
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
 
 /**
  * Sizes
@@ -27,8 +68,9 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+camera.position.set(1, 1, 5)
 scene.add(camera)
+
 
 /**
  * Renderer
