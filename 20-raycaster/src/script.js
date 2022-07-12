547a834a8985b7446b940de0599aa781587cd743
s-cryptor <grisga2001@gmail.com>
2022-07-12 12:46:11 +0300

    test raycaster, play with hovering

--- 20-raycaster/src/script.js
+++ 20-raycaster/src/script.js
@@ -16,48 +16,74 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Mouse
+ */
+const mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX / sizes.width) * 2 - 1
+  mouse.y = -(event.clientY / sizes.height) * 2 + 1
+})
+
+/**
  * Objects
  */
 const object1 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
 )
-object1.position.x = - 2
+object1.position.x = -2
 
 const object2 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
 )
 
 const object3 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
 )
 object3.position.x = 2
 
 scene.add(object1, object2, object3)
 
 /**
+ * Raycaster
+ */
+const raycaster = new THREE.Raycaster()
+
+// const rayOrigin = new THREE.Vector3(-3, 0, 0)
+// const rayDirection = new THREE.Vector3(10, 0, 0)
+// rayDirection.normalize()
+
+// raycaster.set(rayOrigin, rayDirection)
+
+// const intersect = raycaster.intersectObject(object2)
+// console.log(intersect)
+
+// const intersects = raycaster.intersectObjects([object1, object2, object3])
+// console.log(intersects)
+
+/**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -76,7 +102,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -86,18 +112,94 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+let currentIntersect = null
 
-    // Update controls
-    controls.update()
+window.addEventListener('click', () => {
+  if (currentIntersect) {
+    switch (currentIntersect.object) {
+      case object1:
+        console.log('click on object 1')
+        break
 
-    // Render
-    renderer.render(scene, camera)
+      case object2:
+        console.log('click on object 2')
+        break
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+      case object3:
+        console.log('click on object 3')
+        break
+    }
+  }
+})
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+
+  // Animate objects
+  object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
+  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
+  object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
+
+  // Cast a ray
+  //   const rayOrigin = new THREE.Vector3(-3, 0, 0)
+  //   const rayDirection = new THREE.Vector3(1, 0, 0)
+  //   rayDirection.normalize()
+
+  //   raycaster.set(rayOrigin, rayDirection)
+
+  //   const objectsToTest = [object1, object2, object3]
+  //   const intersects = raycaster.intersectObjects(objectsToTest)
+  //   console.log(intersects)
+
+  //   for (const object of objectsToTest) {
+  //     object.material.color.set('#ff0000')
+  //   }
+
+  //   for (const intersect of intersects) {
+  //     intersect.object.material.color.set('#0000ff')
+  //   }
+
+  //   raycaster.setFromCamera(mouse, camera)
+
+  //   const objectsToTest = [object1, object2, object3]
+  //   const intersects = raycaster.intersectObjects(objectsToTest)
+
+  //   for (const intersect of intersects) {
+  //     intersect.object.material.color.set('#0000ff')
+  //   }
+
+  //   for (const object of objectsToTest) {
+  //     if (!intersects.find((intersect) => intersect.object === object)) {
+  //       object.material.color.set('#ff0000')
+  //     }
+  //   }
+
+  raycaster.setFromCamera(mouse, camera)
+  const objectsToTest = [object1, object2, object3]
+  const intersects = raycaster.intersectObjects(objectsToTest)
+
+  if (intersects.length) {
+    if (!currentIntersect) {
+      console.log('mouse enter')
+    }
+
+    currentIntersect = intersects[0]
+  } else {
+    if (currentIntersect) {
+      console.log('mouse leave')
+    }
+
+    currentIntersect = null
+  }
+
+  // Update controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
