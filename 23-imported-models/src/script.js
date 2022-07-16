e94be06018668d88c3355e36e9c795aceef15135
s-cryptor <grisga2001@gmail.com>
2022-07-16 14:00:49 +0300

    import gltf model and animate it

--- 23-imported-models/src/script.js
+++ 23-imported-models/src/script.js
@@ -2,12 +2,59 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
 /**
  * Base
  */
 // Debug
 const gui = new dat.GUI()
+
+/**
+ * Models
+ */
+const gltfLoader = new GLTFLoader()
+const dracoLoader = new DRACOLoader()
+
+// gltfLoader.load(
+//   '/models/Duck/glTF/Duck.gltf',
+//   (gltf) => {
+//     scene.add(gltf.scene.children[0])
+//   },
+//   (progress) => {
+//     console.log('progress', progress)
+//   },
+//   (error) => {
+//     console.log('error', error)
+//   }
+// )
+// gltfLoader.load('/models/FlightHelmet/glTF/FlightHelmet.gltf', (gltf) => {
+//   const children = [...gltf.scene.children]
+//   for (const child of children) {
+//     scene.add(child)
+//   }
+// })
+
+// gltfLoader.load('/models/Duck/glTF-Draco/Duck.gltf', (gltf) => {
+//   scene.add(gltf.scene.children[0])
+// })
+
+let mixer = null
+let fox = null
+
+gltfLoader.load('/models/Fox/glTF/Fox.gltf', (gltf) => {
+  gltf.scene.scale.set(0.025, 0.025, 0.025)
+  scene.add(gltf.scene)
+  fox = gltf.scene
+
+  mixer = new THREE.AnimationMixer(gltf.scene)
+  const action = mixer.clipAction(gltf.animations[2])
+  action.play()
+})
+
+dracoLoader.setDecoderPath('/draco/')
+gltfLoader.setDRACOLoader(dracoLoader)
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -19,15 +66,15 @@ const scene = new THREE.Scene()
  * Floor
  */
 const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
-    new THREE.MeshStandardMaterial({
-        color: '#444444',
-        metalness: 0,
-        roughness: 0.5
-    })
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshStandardMaterial({
+    color: '#444444',
+    metalness: 0,
+    roughness: 0.5
+  })
 )
 floor.receiveShadow = true
-floor.rotation.x = - Math.PI * 0.5
+floor.rotation.x = -Math.PI * 0.5
 scene.add(floor)
 
 /**
@@ -40,10 +87,10 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
 directionalLight.castShadow = true
 directionalLight.shadow.mapSize.set(1024, 1024)
 directionalLight.shadow.camera.far = 15
-directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.left = -7
 directionalLight.shadow.camera.top = 7
 directionalLight.shadow.camera.right = 7
-directionalLight.shadow.camera.bottom = - 7
+directionalLight.shadow.camera.bottom = -7
 directionalLight.position.set(5, 5, 5)
 scene.add(directionalLight)
 
@@ -51,23 +98,22 @@ scene.add(directionalLight)
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -75,7 +121,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(2, 2, 2)
+camera.position.set(5, 5, 5)
 scene.add(camera)
 
 // Controls
@@ -87,7 +133,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas
 })
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
@@ -100,20 +146,30 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 const clock = new THREE.Clock()
 let previousTime = 0
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - previousTime
-    previousTime = elapsedTime
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+  const deltaTime = elapsedTime - previousTime
+  previousTime = elapsedTime
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+  // Update animations
+  if (mixer) {
+    mixer.update(deltaTime)
+  }
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  if (fox) {
+    fox.position.z += deltaTime * 10
+    camera.position.z += deltaTime * 10
+    camera.lookAt(fox.position)
+  }
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
